6b3d131a6823d3bb947f87c14635d2318a578977
GustavAbrahamsson <gustav.abrahamsson@outlook.com>
2021-03-18 21:57:47 +0100

    Test

--- Transmitter-1/src/main.cpp
+++ Transmitter-1/src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
 
+#define JS1_X A0
+#define JS1_Y A1
+#define JS2_X A2
+#define JS2_Y A3
+
+
+int js1_x = 0;
+int js1_y = 0;
+int js2_x = 0;
+int js2_y = 0;
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Program started");
+
+  pinMode(JS1_X, INPUT);
+  pinMode(JS1_Y, INPUT);
+  pinMode(JS2_X, INPUT);
+  pinMode(JS2_Y, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  delay(10);
+
+  js1_x = analogRead(JS1_X);
+  js1_y = analogRead(JS1_Y);
+  js2_x = analogRead(JS2_X);
+  js2_y = analogRead(JS2_Y);
+
+  Serial.println("1X = " + js1_x);
+  Serial.println("1Y = " + js1_y);
+  Serial.println("2X = " + js2_x);
+  Serial.println("2Y = " + js2_y);
+  
+  Serial.println();
 }
